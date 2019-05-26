2d86e456ab7b55ae48fdf55952348958599966f8
Rupin Gupta <rupin.gupta22@gmail.com>
2019-05-26 12:27:42 +0530

    Initial Commit

---
+++ artableFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  artableFinal
+//
+//  Created by Rupin Gupta on 26/05/19.
+//  Copyright © 2019 Rupin Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
